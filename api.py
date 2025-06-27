d1a2070e590fd8d958327c385551b2becba22fb9
IKEA0x90 <ivanchousKutovoy@gmail.com>
2025-06-27 12:08:49 +0400

    Some random starting files that don't work

---
+++ api.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI
+from endpoints.lobbies import router as lobbies_router
+
+app = FastAPI(title="TRGL")
+
+app.include_router(lobbies_router)
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("api:app", host="0.0.0.0", port=8745, reload=True)
---
+++ models/lobbies.py
@@ -0,0 +1,18 @@
+from pydantic import BaseModel
+from typing import Dict, List
+
+class Lobby(BaseModel):
+    lobby_id: str
+    host_id: str
+
+    lobby_name: str
+    players: List[str] = []  
+    
+    def save(self):
+        with open(f"saves/lobbies/{self.lobby_id}.json", "w") as f:
+            f.write(self.model_dump_json())
+
+    @classmethod
+    def load(cls, lobby_id: str):
+        with open(f"saves/lobbies/{lobby_id}.json") as f:
+            return cls.model_validate_json(f.read())
